a335b9507d96b21ddc81503dbc6584b783b497a8
LucasPiffer <piffer.lucas@gmail.com>
2018-09-25 22:22:39 -0300

    Create App component; Add css to App component; Create index.jsx and load app; Fix type in webpack.config resolve options

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         contentBase: './public'
     },
     resolve: {
-        extension: ['', '.js', '.jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
         }
@@ -23,7 +23,7 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /.js[x]?$/,
+                test: /\.jsx?$/,
                 loader: 'babel-loader',
                 exclude: '/node_modules',
                 query: {
